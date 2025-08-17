7248e8d3b0c190ccb92ea8fe06288b6ab8bcad93
xonder05 <daniel.onderk@gmail.com>
2025-08-17 10:54:11 +0200

    Add: robot description package, (not done yet, contains old xcacro files which need to be cleaner or removed later

---
+++ src/robot_description/launch/rviz_launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+    )
+
+    rviz_config_path = os.path.join(
+        get_package_share_directory('robot_description'),
+        'config',
+        'config.rviz'
+    )
+
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config_path],
+        parameters=[
+            {'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ],
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        rviz,
+    ])
