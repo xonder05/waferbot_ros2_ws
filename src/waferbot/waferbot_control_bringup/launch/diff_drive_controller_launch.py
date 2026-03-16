bc8f9c823e80b27a92be288324559f2c367ef53a
xonder05 <daniel.onderk@gmail.com>
2026-03-16 12:55:28 +0100

    Update: performance optimization and new parameter for controller spawner this is first one of multiple commits that optimize the configurations for performance, because the current state does not allow to run multi robot simulation on average devices, these goals aim to fix it, (the previous amcl commit already had some parameter changes related to optimization) diff drive controller spawner got the publish tf parameter for the same reason, this allows users to drop the ekf node in case performance is an issue and just use the controller odometry instead

--- src/waferbot/waferbot_control_bringup/launch/diff_drive_controller_launch.py
+++ src/waferbot/waferbot_control_bringup/launch/diff_drive_controller_launch.py
@@ -5,9 +5,12 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+from nav2_common.launch import RewrittenYaml
+
 def generate_launch_description():
 
     robot_name_arg = DeclareLaunchArgument("robot_name", default_value="waferbot")
+    enable_odom_tf_arg = DeclareLaunchArgument("enable_odom_tf", default_value="false")
 
     config_file_path = PathJoinSubstitution([
         FindPackageShare("waferbot_control_bringup"),
@@ -15,18 +18,28 @@ def generate_launch_description():
         "_diff_drive_controller.yaml"
     ])
 
+    param_substitutions = {
+        "/**/diff_drive_controller.ros__parameters.enable_odom_tf": LaunchConfiguration("enable_odom_tf"),
+    }
+
+    rewritten_config_file = RewrittenYaml(
+        source_file=config_file_path,
+        param_rewrites=param_substitutions,
+    )
+
     diff_drive_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
         namespace=LaunchConfiguration("robot_name"),
         arguments=[
             "diff_drive_controller",
-            "--param-file", config_file_path,
+            "--param-file", rewritten_config_file,
             "--controller-manager-timeout", "60",
         ],
     )
 
     return LaunchDescription([
         robot_name_arg,
+        enable_odom_tf_arg,
         diff_drive_controller_spawner,
     ])
